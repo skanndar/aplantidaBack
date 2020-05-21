4ec70ac3e7c526f01a926f3e58003623ac02091d
Alex AD <skanndar@gmail.com>
2020-05-21 18:16:59 +0200

    signup

--- helpers/middlewares.js
+++ helpers/middlewares.js
@@ -16,3 +16,18 @@ exports.validationLogin = (req, res, next) => {
   if (!email || !password) next(createError(400));
   else next();
 };
+
+exports.validationSignup = (req, res, next) => {
+  const { agreement, 
+    confirm, 
+    email, 
+    fName, 
+    genre, 
+    lName, 
+    password, } = req.body;
+console.log('req.body :>> ', req.body);
+console.log('agreement, confirm, email, fName, genre, lName, password, :>> ', agreement, confirm, email, fName, genre, lName, password,);
+
+  if (!email || !password || !fName || !lName || !genre) next(createError(400));
+  else next();
+};
